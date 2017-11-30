f97240d8342ec4a3adc09543af0b0b26bd5f0d0b
RASHED <hello@MacBooks-MacBook-Pro.local>
2017-11-30 12:07:42 +0600

    first commit

---
+++ safeApp/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  safeApp
+//
+//  Created by RASHED on 11/29/17.
+//  Copyright © 2017 RASHED. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    @IBOutlet var emailText: UITextField!
+    @IBOutlet var passwordText: UITextField!
+    @IBOutlet var segmentControl: UISegmentedControl!
+    @IBOutlet var actionButton: UIButton!
+    
+    
+    @IBAction func action(_ sender: UIButton) {
+        if emailText.text != "" && passwordText.text != ""
+        {
+            if segmentControl.selectedSegmentIndex == 0  //Login user 
+            {
+                Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!, completion: {(user, error) in
+                    if user != nil {
+                        
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription
+                        {
+                            print(myError)
+                        }
+                        else
+                        {
+                            print("ERROR")
+                        }
+                    }
+                })
+               
+            }
+            else   //Sign up user
+            {
+                Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!, completion: { (user, error) in
+                    if user != nil
+                    {
+                        
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription
+                        {
+                            print(myError)
+                        }
+                        else
+                        {
+                            print("ERROR")
+                        }
+                    }
+                })
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
